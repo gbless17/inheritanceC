10c79756c7cb5a29acf00c7a807ed87441e0e23a
Gbless Ugo <gbless1720@gmail.comgit congig --global user.name Gbless>
2022-05-03 21:36:45 +0100

    Inheritance commit

---
+++ Inheritance/ImagePost.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceC
+{
+    //ImagePost derived from Post and adds a property (ImageURL) and teo constructors
+    internal class ImagePost: InheritanceC.Post
+    {
+        public string ImageURL { get; set; }
+        public ImagePost() { }
+
+        public ImagePost(string title, string sendbyUsername, string imageURL, bool isPublic)
+        {
+            this.ID = GetNextID();
+            this.Title = title; 
+            this.SendByusername = sendbyUsername;
+            this.IsPublic = isPublic;  
+
+            // Propert ImageURL is a member of ImagePost, but not of InheritanceC.Post
+            this.ImageURL = imageURL;   
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace InheritanceC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post post1 = new Post("Thanks for the wishes!!", true, "Donald Carter");
+            Console.WriteLine(post1.ToString());
+            Console.ReadLine(); 
+        
+        }
+    }
+}
